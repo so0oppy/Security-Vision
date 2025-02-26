d3f2f2b0bbad56c148af40c2600b4c3084773589
Subin <80036437+hansb1023@users.noreply.github.com>
2025-02-26 18:43:56 +0900

    Revert "[Feat] 웹캠 대신 영상으로 침입 탐지 테스트 완료 (경고문구, 경고음)" - 용량 큰 영상은 올리지 않음 This reverts commit 406322cc4100eb865cb262109bfbdaab6c3931fd.

--- Project.py
+++ Project.py
@@ -1,11 +1,8 @@
 # 실시간 보안 감시 및 침입 탐지 시스템
 # -----------------------------------
 
-import winsound
 import cv2
 import numpy as np
-from ultralytics import YOLO
-import time
 
 # YOLO 모델 로드
 net = cv2.dnn.readNet("yolov4-tiny.weights", "yolov4-tiny.cfg") # 가중치, 설정파일
@@ -17,65 +14,18 @@ with open("coco.names", "r") as file: # YOLO가 학습된 80개의 객체 이름
     classes = [line.strip() for line in file.readlines()]
 
 # cap = cv2.VideoCapture(0) # 기본 카메라
-video_path = '145-1_cam01_trespass01_place01_day_summer.mp4'
-cap = cv2.VideoCapture(video_path) # 웹캠 대신 영상파일 사용
-# output_path = '%s_output.mp4' % video_path.split('.')[0]
+cap = cv2.VideoCapture("") # 웹캠 대신 영상파일 사용
 
-# 원본 영상의 프레임 속도 가져오기
-fps = cap.get(cv2.CAP_PROP_FPS)
-frame_time = 1.0 / fps  # 초 단위로 변환
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-# VideoWriter 객체 생성
-out = cv2.VideoWriter("output.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height))
-
-frame_skip = 10  # 프레임을 특정 개수 건너뛰고 처리(처리 속도 향상)
-frame_count = 0  # 처리한 프레임의 카운터
-
-while cap.isOpened():
-    start_time = time.time()
+while True:
     ret, frame = cap.read()
     if not ret:
-        print("Frame not read correctly.")
         break
-    
-    frame_count += 1
-
-    # 매 'frame_skip'번째 프레임만 처리
-    if frame_count % frame_skip != 0:
-        continue  # 건너뛰기
-
-    # print("Frame shape:", frame.shape) # Frame shape: (2160, 3840, 3)
-    # print("Frame dtype:", frame.dtype) # Frame dtype: uint8
-
-    # 출력 프레임 축소
-    frame = cv2.resize(frame, (640, 480))
 
     # YOLO 입력 전처리
-    height, width, _ = frame.shape # 프레임의 높이, 너비, 채널(RGB) 값 가져오기
-    # 감시 영역 지정
-    monitor_x1 = int(width * 0.55)
-    monitor_x2 = int(width * 0.3)
-    monitor_y = int(height * 0.4)
-
-    # 감시 영역 시각적으로 표시 (사다리꼴)
-    points = np.array([[0, monitor_y], # 상단 좌측
-                    [monitor_x1, monitor_y], # 상단 우측
-                    [monitor_x2, height], # 하단 우측
-                    [0, height]], # 하단 좌측
-                    dtype=np.int32)
-    cv2.polylines(frame, [points], isClosed=True, color=(0, 0, 255), thickness=2) # 테두리만 그림
-    
-    # 흑백 이미지라면, 컬러(RGB)로 변환
-    if frame.shape[2] == 1:  # 흑백 이미지라면(1채널)
-        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
-
+    height, width, channels = frame.shape # 프레임의 높이, 너비, 채널(RGB) 값 가져오기
     # cv2.dnn.blobFromImage: YOLO가 사용 가능하도록 프레임 정규화(0~1사이 값으로 변환)
     # (416, 416): YOLO의 입력 크기(고정)
-    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), swapRB=True, crop=False) 
-    # print("Blob shape:", blob.shape) # Blob shape: (1, 3, 416, 416)
-    
+    blob = cv2.dnn.blobFromImages(frame, 0.00392, (416, 416), swapRB=True, crop=False) 
     net.setInput(blob) # YOLO에 입력 데이터 전달
     outputs = net.forward(output_layers) # YOLO가 감지한 객체 정보 outputs에 저장
 
@@ -109,27 +59,16 @@ while cap.isOpened():
     # 더 엄격한 필터링 -> 신뢰도↑, IOU↓
     # 더 많은 객체 감지 -> 신뢰도↓, IOU↑
 
-    if len(indexes) > 0:
-        for i in indexes.flatten():
-            x, y, w, h = boxes[i]
-            label = f"{classes[class_ids[i]]}: {confidences[i]:.2f}"
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.putText(frame, label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-            # 침입 감지 조건: 사람이 왼쪽 30%, 높이 50% 이내로 들어오면 감지
-            if (x < monitor_x1 or x < monitor_x2) and (y+h) > monitor_y:
-                cv2.putText(frame, "INTRUSION DETECTED!!", (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2)
-                winsound.Beep(1000, 500) # 경고음
+    for i in indexes.flatten():
+        x, y, w, h = boxes[i]
+        label = f"{classes[class_ids[i]]}: {confidence[i]:.2f}"
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     cv2.imshow("Person Detection", frame)
-    # 프레임 간 실제 지연 시간 계산
-    elapsed_time = time.time() - start_time
-    delay = max(int((frame_time - elapsed_time) * 1000), 1)
 
-    if cv2.waitKey(delay) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    out.write(frame)
 
 cap.release()
-out.release()
 cv2.destroyAllWindows()
